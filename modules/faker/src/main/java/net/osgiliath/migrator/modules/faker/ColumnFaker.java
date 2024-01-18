67a12118603785f0f6ea2a89fe246da4120728bf
Charlie Mordant <cmordant1@gmail.com>
2024-01-18 20:47:32 +0100

    some more javadoc

--- modules/faker/src/main/java/net/osgiliath/migrator/modules/faker/ColumnFaker.java
+++ modules/faker/src/main/java/net/osgiliath/migrator/modules/faker/ColumnFaker.java
@@ -9,9 +9,9 @@ package net.osgiliath.migrator.modules.faker;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,15 +23,29 @@ package net.osgiliath.migrator.modules.faker;
 import net.osgiliath.migrator.core.api.metamodel.model.MetamodelVertex;
 import net.osgiliath.migrator.core.configuration.ColumnTransformationDefinition;
 
+/**
+ * Faker for columns.
+ *
+ * @param <T> the type of the column
+ */
 public class ColumnFaker extends AbstractFaker<Object> {
 
 
-	public ColumnFaker(MetamodelVertex metamodel, ColumnTransformationDefinition columnTransformationDefinition) {
-		super(metamodel, columnTransformationDefinition);
-	}
+    /**
+     * Constructor.
+     *
+     * @param metamodel                      the Vertex metamodel (class)
+     * @param columnTransformationDefinition the column transformation definition
+     */
+    public ColumnFaker(MetamodelVertex metamodel, ColumnTransformationDefinition columnTransformationDefinition) {
+        super(metamodel, columnTransformationDefinition);
+    }
 
-	@Override
-	protected Object evaluateField(Object fieldValue) {
-		return super.fake(fieldValue.toString());
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Object evaluateField(Object fieldValue) {
+        return super.fake(fieldValue.toString());
+    }
 }
